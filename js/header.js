b55c2e383bb1a5709b4858ed30e8ef7d035d73b7
thevivienyang0702 <nee0492@gmail.com>
2022-11-15 23:12:05 +0800

    add Pageup js

--- js/header.js
+++ js/header.js
@@ -3,12 +3,15 @@
 //* Set the width of the side navigation to 250px and the left margin of the page content to 250px and add a black background color to body */
 function openNav() {
     document.getElementById("mySidenav").style.width = "250px";
+    
+   
   
   }
   
   //* Set the width of the side navigation to 0 and the left margin of the page content to 0, and the background color of body to white */
   function closeNav() {
     document.getElementById("mySidenav").style.width = "0";
+   
   
   }
   
@@ -30,4 +33,26 @@ function openNav() {
       }
     });
   }
+
+// -------------- 至頂按鈕 --------------------
+
+// Get the button:
+let mybutton = document.getElementById("myBtn");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
   
--- js/product_tab.js
+++ js/product_tab.js
@@ -24,22 +24,22 @@ function openText(evt, textName) {
 
 
 
-  //--------- Navbar Stik ----------
+  //--------- Navbar Stick ----------
 
   // When the user scrolls the page, execute myFunction
-  window.onscroll = function() {myFunction()};
+  // window.onscroll = function() {myFunction()};
 
   // Get the navbar
-  var navbar = document.getElementById("navbar");
+  // var navbar = document.getElementById("navbar");
 
   // Get the offset position of the navbar
-  var sticky = navbar.offsetTop;
+  // var sticky = navbar.offsetTop;
 
   // Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
-  function myFunction() {
-    if (window.pageYOffset >= sticky) {
-      navbar.classList.add("sticky")
-    } else {
-      navbar.classList.remove("sticky");
-    }
-  }
+  // function myFunction() {
+  //   if (window.pageYOffset >= sticky) {
+  //     navbar.classList.add("sticky")
+  //   } else {
+  //     navbar.classList.remove("sticky");
+  //   }
+  // }
